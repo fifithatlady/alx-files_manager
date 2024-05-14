f4895e8aa132a8546d7f685a0a52fb83e61b4f4d
Fifithatlady <felistasfenyere2224@gmail.com>
2024-05-14 12:31:55 +0000

    files

---
+++ server.js
@@ -0,0 +1,13 @@
+import express from 'express';
+import startServer from './libs/boot.js';
+import injectRoutes from './routes.js';
+import injectMiddlewares from './libs/middlewares.js';
+
+const server = express();
+
+injectMiddlewares(server);
+injectRoutes(server);
+startServer(server);
+
+export default server;
+
